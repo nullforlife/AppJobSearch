df53d4130b6bd4448c5db52f8a82c5852bc2e39b
Oskar Jönsson <jonsson.oskar@live.se>
2016-04-12 13:56:59 +0200

    Initial Commit

---
+++ IT JobSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IT JobSearch
+//
+//  Created by ITHS on 2016-04-12.
+//  Copyright © 2016 Oskar Jönsson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
